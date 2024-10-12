54d1494cdbbaf15a43fe576bb7f4839d0ca991de
tushar-26 <savdiyatushar17@gmail.com>
2024-10-12 15:15:54 +0530

    add

---
+++ database.php
@@ -0,0 +1,21 @@
+<?php 
+
+$db_server = "localhost";
+$db_user = "root";
+$db_password = "";
+$db_name = "plogin-register";
+$conn = "";
+
+$conn = mysqli_connect($db_server,
+                       $db_user,
+                       $db_password,
+                       $db_name
+                    );
+
+if(!$conn){
+    die("not conneted");
+}else{
+    echo "connected";
+}
+
+?>
